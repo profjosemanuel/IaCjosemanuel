276b5d8d1ac610d1483f053f99fe766fbacba270
José Manuel Alvarez <97788933+profjosemanuel@users.noreply.github.com>
2025-06-22 22:11:12 +0200

    Update privateroutetable.tf

--- tareaterraform/privateroutetable.tf
+++ tareaterraform/privateroutetable.tf
@@ -53,11 +53,11 @@
 #}
 
 # Asociar subredes públicas a la tabla de enrutamiento publica
-resource "aws_route_table_association" "public_assoc" {
-count = length(var.public_subnets_cidrs)
-subnet_id = aws_subnet.public[count.index].id
-route_table_id = aws_route_table.public_rt.id
-}
+#resource "aws_route_table_association" "public_assoc" {
+#count = length(var.public_subnets_cidrs)
+#subnet_id = aws_subnet.public[count.index].id
+#route_table_id = aws_route_table.public_rt.id
+#}
 
 # (Opcional) Tabla de enrutamiento privada 
 #resource "aws_route_table" "private_rt" {
